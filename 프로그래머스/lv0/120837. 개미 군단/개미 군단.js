823f3d69252bd64b561873b5bcae0b689c37e13a
deun <devdeun@gmail.com>
2022-12-09 00:14:17 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,12 +1,3 @@
 function solution(hp) {
-    let minCount = hp;
-    const getCount = (a, b) => {
-        let remainder;
-        const first = Math.floor(hp / a);
-        remainder = hp - first * a;
-        const second = Math.floor(remainder / b);
-        remainder -= second * b;
-        return first + second + remainder;
-    }
-    return Math.min(getCount(5, 3), getCount(3, 5));
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + (hp % 5) % 3;
 }
