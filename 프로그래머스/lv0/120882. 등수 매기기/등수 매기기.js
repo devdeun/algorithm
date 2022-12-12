1b9ed85cc0c49b684165731f19c5872b61349106
deun <devdeun@gmail.com>
2022-12-13 00:44:17 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,13 +1,5 @@
 function solution(score) {
-    const averageScore =score.reduce((acc, cur) => {
-        const [english, math] = cur;
-        return [...acc, (english + math) / 2]
-    }, []);
-    const ranking = Array(score.length).fill(1);
-    for (let i = 0; i < score.length; i++) {
-        for (let j = 0; j < score.length; j++) {
-            if (averageScore[i] < averageScore[j]) ranking[i]++;
-        }
-    }
-    return ranking; 
+    const averageScore = score.map(([eng, math]) => (eng + math) / 2);
+    const sortedAverageScore = [...averageScore].sort((a, b) => b - a);
+    return averageScore.map(s => sortedAverageScore.indexOf(s) + 1); 
 }
