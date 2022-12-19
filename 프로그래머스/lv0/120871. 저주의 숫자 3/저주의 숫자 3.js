089ab1875a8b9d2fce274157b039c83143ef029e
deun <devdeun@gmail.com>
2022-12-19 22:52:45 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+        result++;
+        while (result % 3 === 0 || String(result).includes(3)) result++;
+    }
+    return result;
+}
