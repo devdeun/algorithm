666e3b56bec128edfa20ea816ce9b4b9917ea574
deun <devdeun@gmail.com>
2022-12-29 00:23:18 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,7 @@
+function solution(food) {
+    let result = "";
+    for (let i = 1; i < food.length; i++) {
+        result += `${i}`.repeat(Math.floor(food[i] / 2));
+    }
+    return result + "0" + result.split("").reverse().join("");
+}
