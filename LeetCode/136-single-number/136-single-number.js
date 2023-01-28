f1c121f5416e32b4b566bb7f90aaa050a281c31f
deun <devdeun@gmail.com>
2023-01-28 22:44:16 +0900

    Time: 866 ms (5.81%), Space: 43.6 MB (70.22%) - LeetHub

---
+++ LeetCode/136-single-number/136-single-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.lastIndexOf(nums[i]) === nums.indexOf(nums[i])) return nums[i];
+    }
+};
