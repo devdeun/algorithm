f82d6332ee7a6b58618fb8ed033bd47a87ee269e
deun <devdeun@gmail.com>
2023-07-13 14:06:31 +0900

    [unrated] Title: x 사이의 개수, Time: 1.30 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,14 +1,3 @@
 function solution(myString) {
-    const result = [];
-    let length = 0;
-    for (let i = 0; i < myString.length; i++) {
-        if (myString[i] === "x") {
-            result.push(length);
-            length = 0;
-        } else {
-            length++;
-        }
-        if (i === myString.length - 1) result.push(length);
-    }
-    return result;
+   return myString.split("x").map(s => s.length);
 }
