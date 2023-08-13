83b78493bcb8b437ceac805b6883255591d6abc3
deun <devdeun@gmail.com>
2023-08-13 23:28:43 +0900

    [unrated] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,8 @@
+function solution(n, [a, b, c], num_list) {
+    switch(n) {
+        case 1: return num_list.slice(0, b + 1);
+        case 2: return num_list.slice(a);
+        case 3: return num_list.slice(a, b + 1);
+        case 4: return num_list.slice(a, b + 1).filter((_, i) => !(i % c));
+    }
+}
