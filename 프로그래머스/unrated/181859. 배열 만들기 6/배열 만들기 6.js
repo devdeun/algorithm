2f5f7057e13022e947a6f56ef23f88e54c0926f3
deun <devdeun@gmail.com>
2023-09-27 00:13:00 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.64 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+    while (i < arr.length) {
+        if (stk.length && stk[stk.length - 1] === arr[i]) {
+            stk.pop();
+            i++;
+        } else {
+            stk.push(arr[i++]);
+        }
+    }
+    return stk.length ? stk : [-1];
+}
