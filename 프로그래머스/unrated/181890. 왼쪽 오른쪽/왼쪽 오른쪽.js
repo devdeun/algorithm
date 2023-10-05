462f661081af0d244a1692732991c1fd3891c316
deun <devdeun@gmail.com>
2023-10-05 23:33:04 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,7 +1,6 @@
 function solution(str_list) {
-    const lIndex = str_list.indexOf("l") > -1 ? str_list.indexOf("l"): 20;
-    const rIndex = str_list.indexOf("r") > -1 ? str_list.indexOf("r"): 20;
+    const lIndex = str_list.indexOf("l") > -1 ? str_list.indexOf("l"): str_list.length;
+    const rIndex = str_list.indexOf("r") > -1 ? str_list.indexOf("r"): str_list.length;
     if (lIndex === -1 && rIndex === -1) return [];
-    console.log(lIndex, rIndex);
     return lIndex < rIndex ? str_list.slice(0, lIndex) : str_list.slice(rIndex + 1);
 }
