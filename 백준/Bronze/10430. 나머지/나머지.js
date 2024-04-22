0390de5b2b36c8bfed6ef09f13c4dc894ddb730e
deun <devdeun@gmail.com>
2024-04-22 17:46:01 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const [a, b, c] = input.map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
