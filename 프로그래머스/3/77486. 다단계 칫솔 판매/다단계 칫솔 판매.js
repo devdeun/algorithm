2409d86cc7fca3903f9cdafc63ce21380fb49dad
deun <devdeun@gmail.com>
2024-09-20 21:17:14 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 33.03 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,22 +1,23 @@
 function solution(enroll, referral, seller, amount) {
-    const PRICE = 100;
+    const PRODUCT_PRICE = 100;
+    const COMMISSION_RATE = 0.1;
     const earnings = Array(enroll.length).fill(0);
     const enrollMap = new Map();
     enroll.forEach((name, index) => enrollMap.set(name, index));
     
-    const setEarnings = (price, sellerIndex) => {
+    const distributeEarnings = (profit, sellerIndex) => {
         if (sellerIndex === undefined) return;
         
-        const fee = Math.floor(price * 0.1);
-        earnings[sellerIndex] += price - fee;
+        const commission = Math.floor(profit * COMMISSION_RATE);
+        earnings[sellerIndex] += profit - commission;
         
         const referralIndex = enrollMap.get(referral[sellerIndex]);
-        if (fee) setEarnings(fee, referralIndex);
+        if (commission) distributeEarnings(commission, referralIndex);
     }
     
     seller.forEach((currentSeller, index) => {
         const sellerIndex = enrollMap.get(currentSeller);
-        setEarnings(amount[index] * PRICE, sellerIndex);
+        distributeEarnings(amount[index] * PRODUCT_PRICE, sellerIndex);
     });
     
     return earnings;
