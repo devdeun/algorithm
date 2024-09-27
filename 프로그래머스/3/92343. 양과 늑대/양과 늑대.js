da1a4dffa28d6053e7a54150752aa88756ec59ab
deun <devdeun@gmail.com>
2024-09-27 09:36:55 +0900

    [level 3] Title: 양과 늑대, Time: 9.75 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -1,42 +1,21 @@
 function solution(info, edges) {
-    const graph = Array.from(Array(info.length), () => Array(info.length).fill(null))
-    const visited = Array.from(Array(info.length), () => Array(info.length).fill(false))
-    let maxTotal = 0
-    
+    const graph = Array.from(Array(info.length), () => Array())
     edges.forEach(([a, b]) => {
-        graph[a][b] = info[b] === 0 ? 1 : -1
-        graph[b][a] = 0
-        if (a === 0) visited[b][a] = true 
+        graph[a].push(b)
     })
     
-    console.log(graph)
-    
-//     const dfs = (v, total) => {
-//         if (v === info.length) return total
+    let maxSheep = 0
+    const dfs = (vertex, sheep, wolf, prevVisitable = []) => {
+        info[vertex] === 0 ? sheep++ : wolf++
         
-//         let maxTotal = 0
-//         for (let i = 0; i < info.length; i++) {
-//             const value = graph[v][i]
-            
-//             if (value === null) continue
-//             if (total + value <= 0) continue
-            
-//             if (!visited[v][i]) {
-//                 visited[v][i] = true
-//                 maxTotal = Math.max(maxTotal, dfs(i ,total + value))
-//             } else {
-//                 maxTotal = Math.max(maxTotal, dfs(i ,total))
-//             }
-            
-//             if (!(v === 0 && i === 0)) visited[v][i] = false
-//         }
+        if (sheep - wolf <= 0) return
+        maxSheep = Math.max(maxSheep, sheep)
         
-//         return maxTotal
-//     }
-    
-    for (let i = 0; i < graph.length; i++) {
-        // const total = dfs(i, 1)
+        const visitable = [...prevVisitable.filter(v => v !== vertex), ...graph[vertex]]
+        visitable.forEach(v => dfs(v, sheep, wolf, visitable))
     }
     
-    return;
+    dfs(0, 0, 0)
+    
+    return maxSheep
 }
