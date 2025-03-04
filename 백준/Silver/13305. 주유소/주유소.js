fe5614363557bab3d20196e356e11442f99f34fe
deun <devdeun@gmail.com>
2025-03-04 15:25:19 +0900

    [Silver III] Title: 주유소, Time: 232 ms, Memory: 33428 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const distance = input[1].split(" ").map(str => Number(str));
-const price = input[2].split(" ").map(str => Number(str));
+const distance = input[1].split(" ").map(Number);
+const price = input[2].split(" ").map(Number);
 
-let totalPrice = distance[0] * price[0];
-let minPrice = price[0]
+let totalPrice = BigInt(distance[0] * price[0]);
+let minPrice = price[0];
 
 for (let i = 1; i < distance.length; i++) {
   if (price[i] < minPrice) {
     minPrice = price[i];
   } 
-  totalPrice += minPrice * distance[i];
+  totalPrice += BigInt(minPrice * distance[i]);
 }
 
-console.log(totalPrice)
+console.log(String(totalPrice));
