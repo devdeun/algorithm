86653f04711cc95656170b3f6b9b931039520038
deun <devdeun@gmail.com>
2025-03-31 21:17:39 +0900

    [Silver III] Title: 소수 구하기, Time: 196 ms, Memory: 24568 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,15 +1,22 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString();
-const [m, n] = input.split(" ").map(Number);
+const fs = require("fs")
+const input = fs.readFileSync("/dev/stdin").toString()
+const [m, n] = input.split(" ").map(Number)
 
-const isPrime = (num) => {
-  if (num === 0 || num === 1) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
+const primeCheck = Array(n + 1)
+const prime = []
+primeCheck[0] = 1
+primeCheck[1] = 1
+
+for (let i = 2; i * i <= n; i++) {
+  if (!primeCheck[i]) {
+    for (let j = i * i; j <= n; j += i) {
+      primeCheck[j] = 1
+    }
   }
-  return true;
 }
 
 for (let i = m; i <= n; i++) {
-  if (isPrime(i)) console.log(i);
+  if (!primeCheck[i]) prime.push(i)
 }
+
+console.log(prime.join('\n'))
