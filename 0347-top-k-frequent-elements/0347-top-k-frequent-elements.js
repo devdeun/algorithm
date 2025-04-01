9b116781ed76cc6305e9f7b1a4b4f50b3ec6e576
deun <devdeun@gmail.com>
2025-04-01 10:50:50 +0900

    Time: 3 ms (99.03%), Space: 58.8 MB (51.34%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const map = new Map()    
+
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1)
+    }
+
+    const sorted = [...map].sort((a, b) => b[1] - a[1])
+    return sorted.slice(0, k).map(arr => arr[0])
+};
