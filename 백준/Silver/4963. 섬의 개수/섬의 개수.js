e6484180411af63a43c1bb0deeb2db058a2cd598
deun <devdeun@gmail.com>
2025-04-07 11:15:10 +0900

    [Silver II] Title: 섬의 개수, Time: 156 ms, Memory: 10616 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.js
@@ -0,0 +1,37 @@
+const fs = require("fs")
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(s => s.split(" ").map(Number))
+
+const dx = [-1, 0, 1, -1, 1, -1, 0, 1]
+const dy = [-1, -1, -1, 0, 0, 1, 1, 1]
+
+const dp = (map, i, j) => {
+  if (!map[i][j]) return
+
+  map[i][j] = 0
+  for (let k = 0; k < dx.length; k++) {
+    const x = i + dx[k]
+    const y = j + dy[k]
+
+    if (0 <= x && 0 <= y && x < map.length && y < map[0].length) {
+      dp(map, x, y)
+    }
+  }
+}
+
+for (let i = 0; i < input.length;) {
+  const [w, h] = input[i]
+  if (w === 0 && h === 0) break
+  const map = input.slice(i + 1, i + 1 + h)
+
+  let count = 0;
+  for (let x = 0; x < h; x++) {
+    for (let y = 0; y < w; y++) {
+      if (!map[x][y]) continue
+      dp(map, x, y)
+      count++
+    }
+  }  
+
+  console.log(count)
+  i += h + 1
+}
